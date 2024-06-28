48a0e7d009e9e9da378e14799f8e11a5a6246e5a
Khizer Ali <tariqaptech9@gmail.com>
2024-06-28 20:46:42 +0500

    update landing page

--- wp-content/plugins/gsapAnimation/js/script.js
+++ wp-content/plugins/gsapAnimation/js/script.js
@@ -1,21 +1,26 @@
 gsap.registerPlugin(ScrollTrigger);
 
-
 const showDemo = () => {
- 
   document.scrollingElement.scrollTo(0, 0);
-  gsap.to(document.querySelector('.loader'), { autoAlpha: 0 });
+  gsap.to(document.querySelector(".loader"), { autoAlpha: 0 });
 
-  gsap.utils.toArray('.section').forEach((section, index) => {
-    const w = section.querySelector('h2');
-    const [x, xEnd] = index % 2 ? ['100%', (w.scrollWidth - section.offsetWidth) * -1] : [w.scrollWidth * -1, 0];
-    gsap.fromTo(w, { x }, {
-      x: xEnd,
-      scrollTrigger: {
-        trigger: section,
-        scrub: 0.5 } });
-
-
+  gsap.utils.toArray(".section").forEach((section, index) => {
+    const w = section.querySelector("h2");
+    const [x, xEnd] =
+      index % 2
+        ? ["100%", (w.scrollWidth - section.offsetWidth) * -0.4]
+        : [w.scrollWidth * -0.4, 0];
+    gsap.fromTo(
+      w,
+      { x },
+      {
+        x: xEnd,
+        scrollTrigger: {
+          trigger: section,
+          scrub: 5,
+        },
+      }
+    );
   });
 };
 
@@ -23,15 +28,56 @@ showDemo();
 
 const boxes = gsap.utils.toArray(".box");
 
-        boxes.forEach((box, i) => {
-            gsap.to(box, {
-                scrollTrigger: {
-                    trigger: box,
-                    scrub: true,
-                    end: "+=300"
-                },
-                x: 100,
-                duration: 5, // Duration of the animation
-                delay: i * 5 // Delay based on the index of the box
-            });
-        });
+boxes.forEach((box, i) => {
+  gsap.to(box, {
+    scrollTrigger: {
+      trigger: box,
+      start: "top center",
+      scrub: 5,
+      end: "+=300",
+    },
+    x: 100,
+    duration: 5, // Duration of the animation
+    delay: i * 0.5, // Delay based on the index of the box
+  });
+});
+
+const boxes_up = gsap.utils.toArray(".box-up");
+
+boxes_up.forEach((box_up, i) => {
+  gsap.to(box_up, {
+    scrollTrigger: {
+      trigger: box_up,
+      start: "top center",
+      scrub: 5,
+      end: "+=300",
+    },
+    y: -50,
+    duration: 5, // Duration of the animation
+    delay: i * 0.5, // Delay based on the index of the box
+  });
+});
+
+let revealContainers = document.querySelectorAll(".reveal .elementor-widget-container");
+
+revealContainers.forEach((container) => {
+  let image = container.querySelector("img");
+  let tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: container,
+      toggleActions: "restart none none reset",
+    },
+  });
+
+  tl.set(container, { autoAlpha: 1 });
+  tl.from(container, 1.5, {
+    xPercent: -100,
+    ease: Power2.out,
+  });
+  tl.from(image, 1.5, {
+    xPercent: 100,
+    scale: 1.3,
+    delay: -1.5,
+    ease: Power2.out,
+  });
+});
--- wp-content/plugins/particleJs/js/script.js
+++ wp-content/plugins/particleJs/js/script.js
@@ -1,115 +1,226 @@
 /* ---- particles.js config ---- */
 
-particlesJS("particles-js", {
-    "particles": {
-      "number": {
-        "value": 50,
-        "density": {
-          "enable": true,
-          "value_area": 1000
-        }
-      },
-      "color": {
-        "value": "#ffffff"
-      },
-      "shape": {
-        "type": "image",
-        "stroke": {
-          "width": 0,
-          "color": "#000000"
-        },
-        "polygon": {
-          "nb_sides": 5
-        },
-        "image": {
-          "src": "http://localhost/glowzaweb/wp-content/uploads/2024/06/glowza-logo-1-1.png",
-          "width": 500,
-          "height": 500
-        }
-      },
-      "opacity": {
-        "value": 1,
-        "random": false,
-        "anim": {
-          "enable": false,
-          "speed": 1,
-          "opacity_min": 0.1,
-          "sync": false
-        }
-      },
-      "size": {
-        "value": 15,
-        "random": true,
-        "anim": {
-          "enable": false,
-          "speed": 40,
-          "size_min": 0.1,
-          "sync": false
-        }
-      },
-      "line_linked": {
+particlesJS("particles-js-black", {
+  "particles": {
+    "number": {
+      "value": 80,
+      "density": {
         "enable": true,
-        "distance": 150,
-        "color": "#ffffff",
-        "opacity": 1,
-        "width": 1
-      },
-      "move": {
-        "enable": true,
-        "speed": 2,
-        "direction": "none",
-        "random": false,
-        "straight": false,
-        "out_mode": "out",
-        "bounce": false,
-        "attract": {
-          "enable": false,
-          "rotateX": 600,
-          "rotateY": 1200
-        }
+        "value_area": 1000
       }
     },
-    "interactivity": {
-      "detect_on": "canvas",
-      "events": {
-        "onhover": {
-          "enable": true,
-          "mode": "grab"
-        },
-        "onclick": {
-          "enable": true,
-          "mode": "push"
-        },
-        "resize": true
+    "color": {
+      "value": "#000"
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
       },
-      "modes": {
-        "grab": {
-          "distance": 140,
-          "line_linked": {
-            "opacity": 1
-          }
-        },
-        "bubble": {
-          "distance": 400,
-          "size": 40,
-          "duration": 2,
-          "opacity": 8,
-          "speed": 3
-        },
-        "repulse": {
-          "distance": 200,
-          "duration": 0.4
-        },
-        "push": {
-          "particles_nb": 4
-        },
-        "remove": {
-          "particles_nb": 2
-        }
+      "polygon": {
+        "nb_sides": 5
+      },
+      "image": {
+        "src": "",
+        "width": 500,
+        "height": 500
       }
     },
-    "retina_detect": true
-  });
+    "opacity": {
+      "value": 1,
+      "random": false,
+      "anim": {
+        "enable": false,
+        "speed": 1,
+        "opacity_min": 0.1,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 10,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 40,
+        "size_min": 0.1,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 150,
+      "color": "#000",
+      "opacity": 1,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 2,
+      "direction": "none",
+      "random": false,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": false,
+        "rotateX": 600,
+        "rotateY": 1200
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "grab"
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "push"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 140,
+        "line_linked": {
+          "opacity": 1
+        }
+      },
+      "bubble": {
+        "distance": 400,
+        "size": 40,
+        "duration": 2,
+        "opacity": 8,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 200,
+        "duration": 0.4
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true
+});
+
+// particlesJS("particles-js", {
+//     "particles": {
+//       "number": {
+//         "value": 50,
+//         "density": {
+//           "enable": true,
+//           "value_area": 1000
+//         }
+//       },
+//       "color": {
+//         "value": "#ffffff"
+//       },
+//       "shape": {
+//         "type": "image",
+//         "stroke": {
+//           "width": 0,
+//           "color": "#000000"
+//         },
+//         "polygon": {
+//           "nb_sides": 5
+//         },
+//         "image": {
+//           "src": "http://localhost/glowzaweb/wp-content/uploads/2024/06/glowza-logo-1-1.png",
+//           "width": 500,
+//           "height": 500
+//         }
+//       },
+//       "opacity": {
+//         "value": 1,
+//         "random": false,
+//         "anim": {
+//           "enable": false,
+//           "speed": 1,
+//           "opacity_min": 0.1,
+//           "sync": false
+//         }
+//       },
+//       "size": {
+//         "value": 15,
+//         "random": true,
+//         "anim": {
+//           "enable": false,
+//           "speed": 40,
+//           "size_min": 0.1,
+//           "sync": false
+//         }
+//       },
+//       "line_linked": {
+//         "enable": true,
+//         "distance": 150,
+//         "color": "#ffffff",
+//         "opacity": 1,
+//         "width": 1
+//       },
+//       "move": {
+//         "enable": true,
+//         "speed": 2,
+//         "direction": "none",
+//         "random": false,
+//         "straight": false,
+//         "out_mode": "out",
+//         "bounce": false,
+//         "attract": {
+//           "enable": false,
+//           "rotateX": 600,
+//           "rotateY": 1200
+//         }
+//       }
+//     },
+//     "interactivity": {
+//       "detect_on": "canvas",
+//       "events": {
+//         "onhover": {
+//           "enable": true,
+//           "mode": "grab"
+//         },
+//         "onclick": {
+//           "enable": true,
+//           "mode": "push"
+//         },
+//         "resize": true
+//       },
+//       "modes": {
+//         "grab": {
+//           "distance": 140,
+//           "line_linked": {
+//             "opacity": 1
+//           }
+//         },
+//         "bubble": {
+//           "distance": 400,
+//           "size": 40,
+//           "duration": 2,
+//           "opacity": 8,
+//           "speed": 3
+//         },
+//         "repulse": {
+//           "distance": 200,
+//           "duration": 0.4
+//         },
+//         "push": {
+//           "particles_nb": 4
+//         },
+//         "remove": {
+//           "particles_nb": 2
+//         }
+//       }
+//     },
+//     "retina_detect": true
+//   });
   
   // Particle js orange 
 
@@ -141,7 +252,7 @@ particlesJS("particles-js", {
         }
       },
       "opacity": {
-        "value": 0.1,
+        "value": 0.5,
         "random": false,
         "anim": {
           "enable": false,
@@ -151,7 +262,7 @@ particlesJS("particles-js", {
         }
       },
       "size": {
-        "value": 5,
+        "value": 10,
         "random": true,
         "anim": {
           "enable": false,
@@ -164,7 +275,7 @@ particlesJS("particles-js", {
         "enable": true,
         "distance": 150,
         "color": "#F55C00",
-        "opacity": 0.3,
+        "opacity": 0.5,
         "width": 1
       },
       "move": {
