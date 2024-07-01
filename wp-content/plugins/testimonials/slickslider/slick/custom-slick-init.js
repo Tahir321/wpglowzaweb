971223b7c938af47fbf84996e3b320f36add087b
Khizer Ali <tariqaptech9@gmail.com>
2024-07-01 17:55:48 +0500

    updated header

--- wp-content/plugins/testimonials/slickslider/slick/custom-slick-init.js
+++ wp-content/plugins/testimonials/slickslider/slick/custom-slick-init.js
@@ -14,11 +14,14 @@ jQuery(document).ready(function($) {
 document.addEventListener("scroll", function() {
     var header = document.querySelector("header");
     let logo_text = document.querySelector('.logo-text span');
+    let menu_button = document.querySelector('.menu-button a');
     if (window.scrollY > 50) {
         header.classList.add('sticky-header');
         logo_text.style.color = '#fff';
+        menu_button.style.padding = '20px'
     } else {
         header.classList.remove('sticky-header');
         logo_text.style.color = '#000';
+        menu_button.style.padding = '38px'
     }
 });
